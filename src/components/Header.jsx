8685c728cce79a306eec2ffbc5095488b9394e86
RahulGurjar07 <rahulgurjarmp09@gmail.com>
2024-04-07 00:35:28 +0530

    add upload image functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,73 +1,153 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import Modal from 'react-modal'
-import { useState } from 'react';
-import { IoMdAddCircleOutline } from 'react-icons/io'
-import { HiCamera } from 'react-icons/hi'
-import { AiOutlineClose } from 'react-icons/ai'
-
+import Link from "next/link";
+import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Modal from "react-modal";
+import { useEffect, useRef, useState } from "react";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { HiCamera } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import { app } from "@/firebase";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 
 export default function Header() {
   const { data: session } = useSession();
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef(null);
+const [imageFileUploading, setImageFileUploading] = useState(false);
+
+  function addImageToPost(e) {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  }
+
+  useEffect(() => {
+    if (selectedFile) {
+      uploadImageToStorage();
+    }
+  } , [selectedFile]);
+
+  async function uploadImageToStorage() {
+    setImageFileUploading(true);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + '-' + selectedFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, selectedFile);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+      },
+      (error) => {
+        console.error(error);
+        setImageFileUploading(false)
+        setImageFileUrl(null)
+        setSelectedFile(null)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then
+        ((downloadURL) => {
+          setImageFileUrl(downloadURL);
+          setImageFileUploading(false)
+        })
+      }
+    )
+  }
 
   return (
-    <div className='shadow-sm border-b sticky top-0 bg-white z-30 p-3'>
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
         {/* Logo */}
 
-        <Link href='/' className='hidden lg:inline-flex'>
-          <Image
-            src='/Instagram_logo_black.webp'
-            width={96} height={96}
-          />
-
+        <Link href="/" className="hidden lg:inline-flex">
+          <Image src="/Instagram_logo_black.webp" width={96} height={96} />
         </Link>
 
-        <Link href='/' className='lg:hidden '>
-          <Image
-            src='/800px-Instagram_logo_2016.webp'
-            width={40} height={40}
-          />
-
+        <Link href="/" className="lg:hidden ">
+          <Image src="/800px-Instagram_logo_2016.webp" width={40} height={40} />
         </Link>
 
         {/* serch input */}
 
         <input
-          type='text' placeholder='Search' className='bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]' />
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]"
+        />
 
         {/* menu items */}
 
         {session ? (
-          <div className='flex gap-5 items-center'>
-            <IoMdAddCircleOutline className='text-2xl cursor-pointer transform hover:scale-125 transition duration-300 hover:text-red-600'
-              onClick={() => setIsOpen(true)} />
-            <img src={session.user.image} alt={session.user.name}
-              className='h-10 w-10 rounded-full cursor-pointer'
-              onClick={signOut} />
+          <div className="flex gap-5 items-center">
+            <IoMdAddCircleOutline
+              className="text-2xl cursor-pointer transform hover:scale-125 transition duration-300 hover:text-red-600"
+              onClick={() => setIsOpen(true)}
+            />
+            <img
+              src={session.user.image}
+              alt={session.user.name}
+              className="h-10 w-10 rounded-full cursor-pointer"
+              onClick={signOut}
+            />
           </div>
         ) : (
-          <button onClick={signIn} className='text-sm font-semibold text-blue-500'>Log In</button>
+          <button
+            onClick={signIn}
+            className="text-sm font-semibold text-blue-500"
+          >
+            Log In
+          </button>
         )}
       </div>
 
       {isOpen && (
-        <Modal isOpen={isOpen}
-          className='max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md'
+        <Modal
+          isOpen={isOpen}
+          className="max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
           onRequestClose={() => setIsOpen(false)}
-          ariaHideApp={false}>
-          <div className='flex flex-col justify-center items-center h-[100%]'>
-            <HiCamera className='text-4xl text-gray-400 cursor-pointer' />
+          ariaHideApp={false}
+        >
+          <div className="flex flex-col justify-center items-center h-[100%]">
+            {selectedFile ? (
+              <img
+                onClick={() => setSelectedFile(null)}
+                src={imageFileUrl}
+                alt="Selected Image"
+                className={`w-full max-h-[250px] object-cover cursor-pointer ${imageFileUploading ? 'animate-pulse' : ''}`}
+              />
+            
+            ) : (
+            <HiCamera onClick={() => filePickerRef.current.click()} className="text-4xl text-gray-400 cursor-pointer" />
+            )}
+            <input hidden ref={filePickerRef} type="file" accept="image/*" onChange={addImageToPost} />
           </div>
-          <input type="text" maxLength='150' placeholder='Please enter your caption...' className='m-4 border-none text-center w-full focus:ring-0 outline-none' />
-          <button disabled className='w-full bg-red-600 text-white p-2 shadow-md rounded-lg hover:brightness-105 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100'>Upload Post</button>
-          <AiOutlineClose className='text-2xl text-gray-400 cursor-pointer absolute top-2 right-2 hover:text-red-600' onClick={() => setIsOpen(false)} />
+
+          <input
+            type="text"
+            maxLength="150"
+            placeholder="Please enter your caption..."
+            className="m-4 border-none text-center w-full focus:ring-0 outline-none"
+          />
+
+          <button
+            // disabled
+            className="w-full bg-red-600 text-white p-2 shadow-md rounded-lg hover:brightness-105 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100"
+          >
+            Upload Post
+          </button>
+          <AiOutlineClose
+            className="text-2xl text-gray-400 cursor-pointer absolute top-2 right-2 hover:text-red-600"
+            onClick={() => setIsOpen(false)}
+          />
         </Modal>
       )}
     </div>
-  )
+  );
 }
---
+++ src/firebase.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "insta-clone-next-app.firebaseapp.com",
+  projectId: "insta-clone-next-app",
+  storageBucket: "insta-clone-next-app.appspot.com",
+  messagingSenderId: "982921323740",
+  appId: "1:982921323740:web:5b0392ba131acec881c019",
+  measurementId: "G-9LQHGJJM60"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+// firebase rules for storage
+
+// service firebase.storage {
+//     match /b/{bucket}/o {
+//       match /{allPaths=**} {
+//         allow read;
+//         allow write : if request.resource.size < 2 * 1024 * 1024 && 
+//         request.resource.contentType.matches('image/.*')
+        
+//       }
+//     }
+//   }
