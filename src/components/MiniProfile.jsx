6a0706c1f65e9cd0569bd57175a3169547aecae8
RahulGurjar07 <rahulgurjarmp09@gmail.com>
2024-04-07 22:50:14 +0530

    Make the feed section responsive

---
+++ src/components/MiniProfile.jsx
@@ -0,0 +1,7 @@
+'use client'
+
+export default function MiniProfile() {
+  return (
+    <div>MiniProfile</div>
+  )
+}
---
+++ src/components/Posts.jsx
@@ -0,0 +1,7 @@
+'use client'
+
+export default function Posts() {
+  return (
+    <div>Posts</div>
+  )
+}
