6f498cb5e10a8815378faa6053a5630140c81d8d
RahulGurjar07 <rahulgurjarmp09@gmail.com>
2024-04-08 22:18:41 +0530

    Add like functionality to the post card

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 import { HiOutlineDotsVertical } from "react-icons/hi";
+import LikeSection from "./LikeSection";
 
 export default function Post({ post }) {
   return (
@@ -17,6 +18,8 @@ export default function Post({ post }) {
         alt={post.caption}
         className="object-cover rounded-sm h-96 w-full"
       />
+
+      <LikeSection id={post.id} />
       <p className="p-5 truncate">
         <span className="font-bold mr-2">{post.username}</span>
         {post.caption}
